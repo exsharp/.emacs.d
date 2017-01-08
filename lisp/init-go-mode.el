b4b8f6ccfb175d67bccb9a2c100d2b80cd2c5f44
zfliu <zfliu_6@163.com>
2017-01-08 19:09:58 +0800

    设置speedbar

--- lisp/init-go-mode.el
+++ lisp/init-go-mode.el
@@ -5,7 +5,6 @@
 ;(add-hook 'go-mode-hook (lambda ()(
 (require 'auto-complete)
 ;(require 'go-mode)
-(require 'go-mode-autoloads)
 (add-hook 'before-save-hook 'gofmt-before-save)
 (require 'go-autocomplete)
  ; )))
